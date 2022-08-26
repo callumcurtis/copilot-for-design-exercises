40b9bea7ac58542f377ccee07043ebb5cab1dc81
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2022-08-25 20:57:33 -0700

    Test filling tank clears low gas indicator

--- tests/test_q1.py
+++ tests/test_q1.py
@@ -7,7 +7,7 @@ from copilot_for_design.exercises.q1.low_gas_indicator import LowGasIndicator
 class TestQ1:
     INITIAL_TANK_FILL = 50
 
-    @pytest.fixture()
+    @pytest.fixture
     def tank(self) -> GasTank:
         return GasTank(self.INITIAL_TANK_FILL)
 
@@ -19,12 +19,22 @@ class TestQ1:
         indicator = LowGasIndicator(tank, 51)
         assert indicator.on
 
-    def test_gas_consumption_past_threshold_sets_indicator(self, tank: GasTank):
+    def test_consuming_gas_past_threshold_sets_indicator(self, tank: GasTank):
         indicator = LowGasIndicator(tank, 30)
         tank.consume(21)
         assert indicator.on
 
-    def test_gas_consumption_above_threshold_does_not_set_indicator(self, tank: GasTank):
+    def test_consuming_gas_above_threshold_does_not_set_indicator(self, tank: GasTank):
         indicator = LowGasIndicator(tank, 30)
         tank.consume(20)
         assert not indicator.on
+
+    def test_filling_tank_past_threshold_clears_indicator(self, tank: GasTank):
+        indicator = LowGasIndicator(tank, 60)
+        tank.fill(10)
+        assert not indicator.on
+
+    def test_filling_tank_below_threshold_does_not_clear_indicator(self, tank: GasTank):
+        indicator = LowGasIndicator(tank, 60)
+        tank.fill(9)
+        assert indicator.on
