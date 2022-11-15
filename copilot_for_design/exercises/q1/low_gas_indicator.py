795ea5d69871fb0896cd9997bfa8c04ad6597a70
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2022-11-15 05:55:49 +0000

    Switch language to Java 17 LTS

--- copilot_for_design/exercises/q1/low_gas_indicator.py
+++
@@ -1,9 +0,0 @@
-from copilot_for_design.exercises.q1.gas_tank import GasTank
-
-
-class LowGasIndicator:
-    def __init__(self, gas_tank: GasTank, threshold: int):
-        assert threshold >= 0
-        self._gas_tank = gas_tank
-        self._threshold = threshold
-        self.on = gas_tank.remaining() < threshold
--- tests/README.md
+++
@@ -1,3 +0,0 @@
-# Tests
-
-Tests are for researcher use only and will not be provided to participants.
