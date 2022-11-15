9cbaa413f371b34d933c0aeabe27991187f1f670
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2022-11-15 06:00:09 +0000

    Add exercise 3

---
+++ lib/src/main/java/copilot_for_design/exercises/q3/Drink.java
@@ -0,0 +1,76 @@
+package copilot_for_design.exercises.q3;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+public class Drink {
+    // Required fields
+    private final float volume;
+    private final float temperature;
+    private final String flavor;
+    private final BigDecimal price;
+
+    // Optional fields
+    private final String topping;
+    private final boolean isSeasonal;
+    private final Set<String> countriesWhereAvailable;
+
+    /**
+     * Create a Drink instance with provided required and optional fields
+     * <p>
+     * TODO: allow creating a Drink using any combination of optional parameters
+     * 
+     * @param volume                  the volume of the drink when it is served
+     * @param temperature             the temperature of the drink when it is served
+     * @param flavor                  the drink's flavor
+     * @param price                   the price of the drink
+     * @param topping                 the name of the drink topping
+     * @param isSeasonal              true iff drink is available only in specific seasons
+     * @param countriesWhereAvailable set of country names where the drink is available
+     */
+    public Drink(
+        float volume,
+        float temperature,
+        String flavor,
+        BigDecimal price,
+        String topping,
+        boolean isSeasonal,
+        Set<String> countriesWhereAvailable
+    ) {
+        this.volume = volume;
+        this.temperature = temperature;
+        this.flavor = flavor;
+        this.price = price;
+        this.topping = topping;
+        this.isSeasonal = isSeasonal;
+        this.countriesWhereAvailable = countriesWhereAvailable;
+    }
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public String getTopping() {
+        return topping;
+    }
+
+    public boolean isSeasonal() {
+        return isSeasonal;
+    }
+
+    public Set<String> getCountriesWhereAvailable() {
+        return countriesWhereAvailable;
+    }
+}
---
+++ lib/src/main/java/copilot_for_design/exercises/q3/README.md
@@ -0,0 +1,19 @@
+# Question 3
+
+## Description
+
+Starbuzz is having difficulty implementing the drink creation system in its new mobile app. You have been hired as a contractor to clean up the creation process for instances of their `Drink` class by designing and implementing a new builder class that allows `Drink` to be instantiated using any combination of optional parameters (`topping`, `isSeasonal`, and `countriesWhereAvailable`). In other words, a `Drink` should be able to be created without any optional arguments provided, with all optional arguments provided, or with any other subset of optional arguments provided.
+
+## Criteria
+
+Add a main method to your builder class demonstrating the creation of a `Drink` with the following fields:
+
+* volume: 243.8
+* temperature: 34.6
+* flavor: "coffee"
+* price: 5.78
+* topping: \<not provided\>
+* isSeasonal: true
+* countriesWhereAvailable: \<not provided\>
+
+Any non-provided fields should default to sensible values.
