347916795457865f6d140f941d0110d3320b67c5
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-01-07 01:30:52 +0000

    Add brief GitHub Copilot tutorial

---
+++ lib/src/main/java/copilot_for_design/practice/CopilotTutorial.java
@@ -0,0 +1,34 @@
+package copilot_for_design.practice;
+
+public class CopilotTutorial {
+    
+
+    /* For the following three method descriptions, experiment with GitHub
+     * Copilot by placing your cursor on the next line (indented) after the
+     * method description comment, waiting a second for a suggestion to
+     * appear, and then using one of the following commands:
+     *     TAB: accept the suggestion
+     *     ESC: reject the suggestion
+     *     ⌥+] or Alt+]: see next suggestion
+     *     ⌥+[ or Alt+[: see previous suggestion
+     *
+     * TIP: if a suggestion does not appear, or it is not what you want,
+     * try improving the comment or writing the method signature yourself.
+     * 
+     * NOTE: sometimes there may not be a previous/next suggestion to see.
+    */
+
+
+
+
+
+    // This is a main method to test the methods below
+
+
+    // This method returns the greatest common denominator of two integers.
+    // Example: gcd(12, 8) returns 4.
+
+
+    // This method accepts two strings and returns the number of occurrences of the second string in the first string.
+    // Example: countOccurrences("This is a test", "is") returns 2.
+}
---
+++ lib/src/main/java/copilot_for_design/practice/README.md
@@ -0,0 +1,3 @@
+# Instructions
+
+Follow the instructions in [CopilotTutorial.java](./CopilotTutorial.java) for an introduction to using GitHub Copilot. This is NOT part of the quiz - to complete the quiz, instead see the [exercises](../exercises).
