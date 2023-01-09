f5c317be7747bfbeaa754a5722bc4ee08c9376c3
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-01-09 21:56:01 +0000

    Specify Mesh is immutable

--- lib/src/main/java/copilot_for_design/exercises/q5/Mesh.java
+++ lib/src/main/java/copilot_for_design/exercises/q5/Mesh.java
@@ -2,7 +2,7 @@ package copilot_for_design.exercises.q5;
 
 /**
  * Defines the physical geometry of a type of object in the simulation.
- * Meshes are often identical between objects of the same type.
- * For example, all boulders in the simulation may possess identical meshes.
+ * Meshes are immutable and identical between objects of the same type.
+ * For example, all boulders in the simulation possess identical meshes.
  */
 public interface Mesh {}
