4806c045553788b426d14d15934fd9a868336bc6
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-01-20 04:45:24 +0000

    Add advanced Copilot shortcuts to tutorial

--- lib/src/main/java/copilot_for_design/practice/CopilotTutorial.java
+++ lib/src/main/java/copilot_for_design/practice/CopilotTutorial.java
@@ -1,34 +1,41 @@
 package copilot_for_design.practice;
 
 public class CopilotTutorial {
-    
-
-    /* For the following three method descriptions, experiment with GitHub
-     * Copilot by placing your cursor on the next line (indented) after the
-     * method description comment, waiting a second for a suggestion to
-     * appear, and then using one of the following commands:
-     *     TAB: accept the suggestion
-     *     ESC: reject the suggestion
-     *     ⌥+] or Alt+]: see next suggestion
-     *     ⌥+[ or Alt+[: see previous suggestion
-     *
-     * TIP: if a suggestion does not appear, or it is not what you want,
-     * try improving the comment or writing the method signature yourself.
+    /* 
+     * For the following method descriptions/comments, experiment with
+     * GitHub Copilot by doing the following:
+     *    1) place your cursor on the next line (indented) after the comment
+     *    2) wait a second for a suggestion to appear
+     *    3) use one of the following commands:
+     *        TAB: accept the suggestion
+     *        ESC: reject the suggestion
+     *        CTRL + ENTER: open the Copilot sidebar for more suggestions
+     *        Option (⌥) + ] or Alt + ]: see next suggestion (if available)
+     *        Option (⌥) + [ or Alt + [: see previous suggestion (if available)
      * 
-     * NOTE: sometimes there may not be a previous/next suggestion to see.
-    */
-
-
+     * NOTE: Option (⌥) + \ or ALT + \: triggers a suggestion manually
+     *
+     * TIP1: if a suggestion does not appear right away, try to start writing
+     *   the method yourself.
+     * 
+     * TIP2: if a suggestion appears but is not what you want, try the
+     *   next/previous suggestions and the Copilot sidebar (commands above).
+     */
 
 
 
     // This is a main method to test the methods below
 
 
+
     // This method returns the greatest common denominator of two integers.
     // Example: gcd(12, 8) returns 4.
 
 
+
     // This method accepts two strings and returns the number of occurrences of the second string in the first string.
     // Example: countOccurrences("This is a test", "is") returns 2.
+
+
+
 }
